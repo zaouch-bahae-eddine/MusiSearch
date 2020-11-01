472e425b14211be02bda4873a1fe72c60556e5f1
zaouch-bahae-eddine <zaouch.bahae.eddine@gmail.com>
2020-11-01 09:09:04 +0100

    01/11/20

--- src/component/Content/Services/Service/Service.js
+++ src/component/Content/Services/Service/Service.js
@@ -4,7 +4,7 @@ import style from './Service.module.css';
 function Service(props) {
     return (
         <div className={style.Service}>
-            <img clasName={style.ServiceIcon} src={props.srcIcon} alt="icon-service"/>
+            <img clasName={style.ServiceIcon} src={"../../../../img/servicesIcons/"+props.srcIcon} alt="icon-service"/>
             <h3 className={style.ServiceName}>{props.name}</h3>
             <p className={style.ServiceDescription}>{props.description}</p>
         </div>
--- src/component/Content/Services/Services.js
+++ src/component/Content/Services/Services.js
@@ -4,9 +4,10 @@ import Service from './Service/Service';
 class Services extends Component {
     state={
         dataServices: [
-            {icon:'', name:'radio', description:'tata tata'},
-            {icon:'', name:'lest to music', description:'tata tata'},
-            {icon:'', name:'upload your own', description:'tata tata'},
+            {icon:'radio.png', name:'radio', description:'tata tata'},
+            {icon:'caskbaf.png', name:'lest to music', description:'tata tata'},
+            {icon:'guitare.png', name:'upload your own', description:'tata tata'},
+            {icon:'cd.png', name:'upload your own', description:'tata tata'},
         ]
     }
     render() {
