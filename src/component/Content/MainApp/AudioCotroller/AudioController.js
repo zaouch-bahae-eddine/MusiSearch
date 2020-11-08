c10f2b454a0d9d96f00bdc5b1c1173fff6bb816c
zaouch-bahae-eddine <zaouch.bahae.eddine@gmail.com>
2020-11-09 00:19:26 +0100

    09/11/2020

--- src/component/Content/MainApp/AudioCotroller/AudioController.js
+++ src/component/Content/MainApp/AudioCotroller/AudioController.js
@@ -1,74 +1,118 @@
-import { faLessThanEqual } from '@fortawesome/free-solid-svg-icons';
+import { faLessThanEqual, faSortNumericUpAlt } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import { Component } from 'react';
 import style from './AudioController.module.css';
 
 class AudioController extends Component {
 
-    audio = new Audio(this.props.url);
-    state = {
-        played: false,
-        volume: 0.2,
+    constructor(props){
+        super(props);
+        this.state = {
+            played: false,
+            volume: 0.2,
+            currentTime: 0,
+            duration: 0,
+        };
+        this.timeTrigger = null;
+        this.audio = new Audio(this.props.url);
+        this.audio.preload = "metadata";
+        this.audio.addEventListener('loadedmetadata', ()=>{
+            console.log(this.audio.duration)
+            this.setState({duration: this.audio.duration});
+        });
     }
-    toggelPlayed = ()=>{
+
+
+    switchPlay = () => {
         this.setState((prevState)=>{
             return {played: !prevState.played};
-        })
-    };
-    switchPlay = () => {
-        this.toggelPlayed();
-        this.audio.paused? this.audio.play() : this.audio.pause();
+        });
+        if(this.audio.paused){
+            this.audio.play();
+            this.startTimeReading();
+        }
+        else {
+            this.audio.pause();
+            this.stopTimeReading();
+        }
     }
+
     nextAudio = ()=>{
         this.audio.pause();
-        delete this.audio;
-        this.audio = new Audio(this.props.url);
         this.props.next();
-        this.setState({played: false});
+        this.setState({played: false, duration: this.audio.duration});
     };
     previousAudio = ()=>{
         this.audio.pause();
-        delete this.audio;
-        this.audio = new Audio(this.props.url);
         this.props.previous();
-        this.setState({played: false});
+        this.setState({played: false, duration: this.audio.duration});
     };
+    startTimeReading = ()=>{
+        this.timeTrigger = setInterval(()=>this.setState({currentTime: this.audio.currentTime}), 1000);
+    }
+    stopTimeReading = ()=>{
+        clearInterval(this.timeTrigger);
+    }
+    formatTime = (time) => {
+        var min = Math.floor(time / 60);
+        var sec = Math.floor(time % 60);
+        return min + ':' + ((sec<10) ? ('0' + sec) : sec);
+      }
     componentDidMount() {
+
         this.audio.addEventListener('ended', this.setState(()=>{
             return {played: false};
         }));
+
     }
     componentWillUnmount(){
         this.audio.removeEventListener('ended', this.setState(()=>{
             return {played: false};
         }));
     }
+
     shouldComponentUpdate(nextProps, nextState){
-        if(nextProps.url.localeCompare(this.props.url) != 0 || this.state.played != nextState.played ){
+        const urlCompoared = nextProps.url.localeCompare(this.props.url);
+        if( urlCompoared != 0 ){
+            this.audio.pause();
+            delete this.audio;
+            this.audio = new Audio(nextProps.url);
+        }
+        
+        if( urlCompoared != 0 || 
+        this.state.played != nextState.played || 
+        this.state.currentTime != nextState.currentTime ||
+        this.state.duration != nextState.duration
+        )
+        {
             return true;
         }
         else {
-            console.log('not update');
             return false;
         }
     }
-    componentDidUpdate(){
-        this.audio.addEventListener('ended', this.setState(()=>{
-            return {played: false};
-        }));
+    componentDidUpdate(prevProps, prevState){
+        if(prevProps.url.localeCompare(this.props.url) != 0 ){
+            this.audio.addEventListener('ended', this.setState(()=>{
+                return {played: false};
+            }));
+        }
     }
+    
     render(){
-        this.audio.volume = 0.01;
+
+        console.log('render22');
         return (
             <div className={style.AudioContainer}>
                 <div className={style.AudioButtonControl}>
-                    <div className={style.Previose} onClick={()=>{this.previousAudio();}}>previose</div>
+                    <div className={style.Previose} onClick={()=>{this.previousAudio()}}>previose</div>
                     <div className={style.PlayPause} 
-                    onClick={()=>this.switchPlay()}>
+                    onClick={()=>{this.switchPlay()}}>
                         {!this.state.played? "Play" : "Pause"}
                     </div>
-                    <div className={style.Next} onClick={()=>this.nextAudio()}>next</div>
-                    <div className={style.currentTime}>{this.audio.currentTime}</div>
+                    <div className={style.Next} onClick={()=>{this.nextAudio()}}>next</div>
+                    <div className={style.currentTime}>{this.formatTime(this.state.currentTime)}</div>
+                    <div className={style.duration}>{this.formatTime(this.state.duration)}</div>
                 </div>
             </div>
         );
