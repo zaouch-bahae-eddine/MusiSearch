c20b261bf2f3c59b2250e94117e6fd0e59379066
zaouch-bahae-eddine <zaouch.bahae.eddine@gmail.com>
2020-11-08 01:09:22 +0100

    08/11/2020

--- src/component/Content/MainApp/AudioCotroller/AudioController.js
+++ src/component/Content/MainApp/AudioCotroller/AudioController.js
@@ -1,28 +1,79 @@
+import { faLessThanEqual } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import { Component } from 'react';
 import style from './AudioController.module.css';
-const OnOffAudio = (audio,play) => {
-    if(play){
-        audio.play();
-        alert('play');
+
+class AudioController extends Component {
+
+    audio = new Audio(this.props.url);
+    state = {
+        played: false,
+        volume: 0.2,
     }
-    else {
-        audio.pause();
-        alert('stop');
+    toggelPlayed = ()=>{
+        this.setState((prevState)=>{
+            return {played: !prevState.played};
+        })
+    };
+    switchPlay = () => {
+        this.toggelPlayed();
+        this.audio.paused? this.audio.play() : this.audio.pause();
     }
-}
-const AudioController = (props) => {
-    const audio = new Audio(props.urlMusic);
-    console.log('hi',audio);
-    return (
-        <div className={style.AudioContainer}>
-            <div className={style.AudioButtonControl}>
-                <div className={style.Previose}>previose</div>
-                <div className={style.PlayPause} onClick={()=>{props.toggelPlay(); OnOffAudio(audio, props.play);} }>{!props.play? "Play" : "Pause"}</div>
-                <div className={style.PlayPause}>next</div>
+    nextAudio = ()=>{
+        this.audio.pause();
+        delete this.audio;
+        this.audio = new Audio(this.props.url);
+        this.props.next();
+        this.setState({played: false});
+    };
+    previousAudio = ()=>{
+        this.audio.pause();
+        delete this.audio;
+        this.audio = new Audio(this.props.url);
+        this.props.previous();
+        this.setState({played: false});
+    };
+    componentDidMount() {
+        this.audio.addEventListener('ended', this.setState(()=>{
+            return {played: false};
+        }));
+    }
+    componentWillUnmount(){
+        this.audio.removeEventListener('ended', this.setState(()=>{
+            return {played: false};
+        }));
+    }
+    shouldComponentUpdate(nextProps, nextState){
+        if(nextProps.url.localeCompare(this.props.url) != 0 || this.state.played != nextState.played ){
+            return true;
+        }
+        else {
+            console.log('not update');
+            return false;
+        }
+    }
+    componentDidUpdate(){
+        this.audio.addEventListener('ended', this.setState(()=>{
+            return {played: false};
+        }));
+    }
+    render(){
+        this.audio.volume = 0.01;
+        return (
+            <div className={style.AudioContainer}>
+                <div className={style.AudioButtonControl}>
+                    <div className={style.Previose} onClick={()=>{this.previousAudio();}}>previose</div>
+                    <div className={style.PlayPause} 
+                    onClick={()=>this.switchPlay()}>
+                        {!this.state.played? "Play" : "Pause"}
+                    </div>
+                    <div className={style.Next} onClick={()=>this.nextAudio()}>next</div>
+                    <div className={style.currentTime}>{this.audio.currentTime}</div>
+                </div>
             </div>
-            
-        </div>
-    )
+        );
+    }
+    
 }
 
 export default AudioController;
--- src/component/Content/MainApp/MainApp.js
+++ src/component/Content/MainApp/MainApp.js
@@ -14,7 +14,7 @@ class MainApp extends Component {
             {
                 id:"id-m-2",
                 title:"Darck Silence",
-                url:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/Marshmello%20-%20Silence%20ft.%20Khalid.mp3",
+                url:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/NF%20-%20Let%20You%20Down.mp3",
                 img:"https://instagram.fcdg1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/120845218_150086493444404_1330499283871454531_n.jpg?_nc_ht=instagram.fcdg1-1.fna.fbcdn.net&_nc_ohc=3JtXpoXt_wIAX8oeKjg&oh=d76f59e1260bb45d3873ad71e9523367&oe=5FC97CCF",
                 artist:"bahae-eddine zaouch",
                 albom:"albom: tyarra",
@@ -22,30 +22,45 @@ class MainApp extends Component {
             {
                 id:"id-m-1",
                 title:"Darck Silence",
-                url:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/Marshmello%20-%20Silence%20ft.%20Khalid.mp3",
+                url:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/Post%20Malone%20-%20rockstar%20ft.%2021%20Savage%20(1).mp3",
                 img:"https://instagram.fcdg1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/120845218_150086493444404_1330499283871454531_n.jpg?_nc_ht=instagram.fcdg1-1.fna.fbcdn.net&_nc_ohc=3JtXpoXt_wIAX8oeKjg&oh=d76f59e1260bb45d3873ad71e9523367&oe=5FC97CCF",
                 artist:"bahae-eddine zaouch",
                 albom:"albom: tyarra",
             },
         ],
-        play: false,
+        current: 0,
     }
-    toggelPlay = ()=>{
-        this.setState((preState)=>{
-            return {play: !preState.play}
+    next = ()=>{
+        this.setState((previousState)=>{
+            if( previousState.current < previousState.audios.length - 1){
+                return {current: previousState.current + 1};
+            } else {
+                return {current: 0};
+            }
         });
-    }
+    };
+    previous = ()=>{
+        this.setState((previousState)=>{
+            if( previousState.current > 0 ){
+                return {current: previousState.current - 1};
+            } else {
+                return {current: 0};
+            }
+        });
+    };
+
     render() {
+        console.log('render')
         return (
             <div>
                 <AudioController 
-                title={this.state.audios[0].title} 
-                urlMusic={this.state.audios[0].url} 
-                imgMusic={this.state.audios[0].title} 
-                artist={this.state.audios[0].url} 
-                albom={this.state.audios[0].albom}
-                toggelPlay = {this.toggelPlay}
-                play = {this.state.play}
+                title={this.state.audios[this.state.current].title} 
+                url={this.state.audios[this.state.current].url} 
+                img={this.state.audios[this.state.current].title} 
+                artist={this.state.audios[this.state.current].url} 
+                albom={this.state.audios[this.state.current].albom}
+                next = {this.next}
+                previous = {this.previous}
                 />
             </div>
         )
